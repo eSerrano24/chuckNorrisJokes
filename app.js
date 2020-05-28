65a3c4e19b958b3cc619ae060df520140c35f6a3
Eric <eric@eric.home>
2020-05-27 19:02:51 -0700

    Added <ul> to display jokes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.querySelector(".get-jokes").addEventListener("click", getJokes);
 
 function getJokes(e) {
-  const number = document.querySelector('input[type="number"]').nodeValue;
+  const num = document.querySelector('input[type="number"]').value;
 
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${num}`, true);
 
   xhr.onload = function () {
     if (this.status === 200) {
